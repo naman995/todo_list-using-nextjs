44a12725567a08a647d68959b4feac691127eb70
naman995 <rajputnaman823>
2023-09-27 18:47:27 +0530

    To list complete

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,51 @@ import React, { useState } from "react";
 const page = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
+  const [mainTask, setMainTask] = useState([]);
+
   const submitHandler = (e) => {
     e.preventDefault();
+    setMainTask([...mainTask, { title, desc }]);
     setTitle("");
     setDesc("");
+    console.log(mainTask);
   };
+
+  const deleteHandler = (i) => {
+    let copyTask = [...mainTask];
+    copyTask.splice(i, 1);
+    setMainTask(copyTask);
+  };
+
+  let renderTask = <h2 className="text-2xl font-bold">No Task Available</h2>;
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((t, i) => {
+      return (
+        <div
+          key={i}
+          className="flex  justify-between bg-slate-200 rounded-2xl p-4 mt-4"
+        >
+          <div className="flex place-item-center">
+            <h1 className="pr-10 my-auto">Task {i + 1}</h1>
+            <h5 className="font-semibold text-xl my-auto">{t.title}</h5>
+            <h2 className="pl-10 my-auto">{t.desc}</h2>
+          </div>
+          <div>
+            <button
+              onClick={()=>deleteHandler(i)}
+              className="bg-red-600 text-white p-4 rounded-lg ml-4 font-extrabold"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      );
+    });
+  }
+
   return (
     <>
-      <h1 className="bg-black text-white text-5xl text-center p-5 font-bold">
+      <h1 className="bg-black text-white text-5xl text-center p-5 font-bold ">
         TODO lists
       </h1>
       <form
@@ -33,10 +70,14 @@ const page = () => {
           onChange={(e) => setDesc(e.target.value)}
         />
 
-        <button className="bg-black text-white p-4 rounded-lg ml-4 font-extrabold">
+        <button className="bg-purple-600 text-white p-4 rounded-lg ml-4 font-extrabold">
           Add Task
         </button>
       </form>
+
+      <div className="p-8 mx-[14%]">
+        <ul>{renderTask}</ul>
+      </div>
     </>
   );
 };
